536efd6e6f4f9ad70f7e9d3161e6c8a11281421a
HuTao <92409960+GenshinVideo@users.noreply.github.com>
2025-01-14 11:12:03 +0900

    Add files via upload

--- Other/Send_code_to_Discord.user.js
+++ Other/Send_code_to_Discord.user.js
@@ -4,7 +4,7 @@
 // @description:ja シリアルコードをDiscordに送信するボタンを表示するスクリプト。
 // @author         胡蝶 桜 ( @Sakura Kocho )
 // @namespace      https://genshinvideo.github.io/HuTao/
-// @version        1.0
+// @version        1.1
 // @match          https://genshin.hoyoverse.com/*/gift*
 // @match          https://hsr.hoyoverse.com/gift*
 // @match          https://zenless.hoyoverse.com/redemption*
@@ -21,7 +21,9 @@ var BotProfile = "https://raw.githubusercontent.com/GenshinVideo/HuTao/main/Auto
 var DefaultBotProfileName = "シトラリ";
 var DefaultBotProfileAvatarURL = "https://fastcdn.hoyoverse.com/content-v2/hk4e/127513/e0ea0a8d21fbbc785914e4d2c69c15f2_7699597058027873875.png";
 
+var SendButton;
 function createButton() {
+  SendButton = document.querySelector('.SC2D_Button');
   var button = document.createElement('button');
   button.innerHTML = 'Send<svg xmlns="http://www.w3.org/2000/svg" class="SC2D_Svg" width="18" height="18" viewBox="0 0 640 512"><path d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z"/></svg>';
   button.classList.add("SC2D_Button");
@@ -74,10 +76,11 @@ async function handleButtonClick() {
     var { username, avatar_url } = BotConfig;
 
     await sendToAllWebhooks(message, username, avatar_url);
-    document.querySelector('.SC2D_Button').remove();
+    SendButton.remove();
 //    alert('すべてのWebhookへの送信が完了しました！');
   } catch (error) {
-    alert('Error');
+    console.log(`Error (Unknown)`);
+    SendButton.remove();
   }
 }
 
@@ -134,8 +137,12 @@ async function sendToAllWebhooks(message, username, avatar_url) {
 }
 
 document.addEventListener('click', (event) => {
+  SendButton = document.querySelector('.SC2D_Button');
   const target = event.target;
   if (target.tagName === 'BUTTON' && target.className.includes('cdkey')) {
+    if (SendButton) {
+      SendButton.remove();
+    }
     createButton();
   }
 });
