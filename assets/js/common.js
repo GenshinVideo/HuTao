1319c37790ef88501029110236fa69549b0c1198
HuTao <92409960+GenshinVideo@users.noreply.github.com>
2026-09-24 15:09:54 +0900

    Add files via upload

--- assets/js/common.js
+++ assets/js/common.js
@@ -69,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
     HI3: "https://sdk.hoyoverse.com/bh3/announcement/index.html?game_biz=bh3_global&bundle_id=bh3_jp&game=bh3&platform=pc&uid=10000000&level=100&region=jp01&lang=ja#/",
     EF: "https://ef-webview.gryphline.com/page/game_bulletin?platform=Windows&channel=6&lang=ja-jp&server=2&subChannel=801",
     Material: "Material/",
+    HYVLauncherVideo: "../HYVLauncherVideo/",
     Resource: "Resource/",
     AutoSign: "AutoSign/",
     AutoSign2: "AutoSign2/",
